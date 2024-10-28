cb30364a5665a2981776fda1fdc5ccb877f60aad
umeshgandla <139842606+umeshgandla@users.noreply.github.com>
2024-10-27 21:11:09 -0400

    Update variables.tf

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,24 +1,29 @@
-# Define variables for the configuration
-variable "aws_region" {
-  description = "AWS region to deploy resources"
+variable "region" {
+  description = "AWS region"
   type        = string
-  default     = "us-east-1" 
+  default     = "us-east-2"
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "CIDR block for main VPC"
   type        = string
   default     = "10.100.0.0/16"
 }
 
 variable "subnet_cidr" {
-  description = "CIDR block for the subnet"
+  description = "CIDR block for main subnet"
   type        = string
   default     = "10.100.1.0/24"
 }
 
 variable "other_vpc_cidr" {
-  description = "CIDR block for the other VPC"
+  description = "CIDR block for the peered VPC"
   type        = string
   default     = "10.200.0.0/16"
+}
+
+variable "other_subnet_cidr" {
+  description = "CIDR block for the subnet in the peered VPC"
+  type        = string
+  default     = "10.200.1.0/24"
 }
