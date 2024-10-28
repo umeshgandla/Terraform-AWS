f4c5ca33282dd407c2be72165cf89b249618c85a
umeshgandla <139842606+umeshgandla@users.noreply.github.com>
2024-10-27 21:12:36 -0400

    Update outputs.tf

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,29 +1,19 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.main_vpc.id
+output "main_vpc_id" {
+  value = aws_vpc.main_vpc.id
 }
 
-output "subnet_id" {
-  description = "The ID of the subnet"
-  value       = aws_subnet.main_subnet.id
+output "other_vpc_id" {
+  value = aws_vpc.other_vpc.id
 }
 
-output "internet_gateway_id" {
-  description = "The ID of the Internet Gateway"
-  value       = aws_internet_gateway.main_igw.id
+output "main_subnet_id" {
+  value = aws_subnet.main_subnet.id
 }
 
-output "security_group_id" {
-  description = "The ID of the Security Group"
-  value       = aws_security_group.main_sg.id
+output "other_subnet_id" {
+  value = aws_subnet.other_subnet.id
 }
 
-output "network_acl_id" {
-  description = "The ID of the Network ACL"
-  value       = aws_network_acl.main_acl.id
-}
-
-output "guardduty_detector_id" {
-  description = "The ID of the GuardDuty detector"
-  value       = aws_guardduty_detector.guardduty.id
+output "vpc_peering_connection_id" {
+  value = aws_vpc_peering_connection.peer_connection.id
 }
