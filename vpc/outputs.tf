940f685f423d5d9e7941fa2527651a3007a7361d
umeshgandla <139842606+umeshgandla@users.noreply.github.com>
2024-10-27 21:07:34 -0400

    Update outputs.tf

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,22 +1,29 @@
-# Output VPC IDs
-output "main_vpc_id" {
-  value = aws_vpc.main_vpc.id
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main_vpc.id
 }
 
-output "other_vpc_id" {
-  value = aws_vpc.other_vpc.id
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = aws_subnet.main_subnet.id
 }
 
-# Output Security Group ID
-output "main_security_group_id" {
-  value = aws_security_group.main_sg.id
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.main_igw.id
 }
 
-# Output Route Table IDs
-output "main_route_table_id" {
-  value = aws_route_table.main_route_table.id
+output "security_group_id" {
+  description = "The ID of the Security Group"
+  value       = aws_security_group.main_sg.id
 }
 
-output "other_vpc_route_table_id" {
-  value = aws_route_table.other_vpc_route_table.id
+output "network_acl_id" {
+  description = "The ID of the Network ACL"
+  value       = aws_network_acl.main_acl.id
+}
+
+output "guardduty_detector_id" {
+  description = "The ID of the GuardDuty detector"
+  value       = aws_guardduty_detector.guardduty.id
 }
